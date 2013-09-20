b7e8be6d90060eb918828efe98639dffd3f16a61
r. clayton <rclayton@monmouth.edu>
2013-09-19 21:48:14 -0400

    drop scala mode

--- init.el
+++ init.el
@@ -235,34 +235,6 @@
   (add-to-list 'auto-mode-alist '("\\.pypic$" . python-mode))
 
 
-; scala
-
-  ; Clone https://github.com/haxney/scala-mode.git , but it doesn't define
-  ; scala-mode-audo , which can be found at
-  ; lampsvn.epfl.ch/svn-repos/scala/scala-tool-support/trunk/src/emacs/scala-mode-auto.el
-  ; .
-
-  (when (require-or-print 'scala-mode-auto)
-
-    (add-hook 'scala-mode-hook
-      (lambda ()
-	(scala-mode-feature-electric-mode)
-
-	(local-set-key "\C-csiri" 'scala-run-scala)
-	(local-set-key "\C-csiti" 'scala-switch-to-interpreter)
-	(local-set-key "\C-csier" 'scala-eval-region)
-	(local-set-key "\C-csieb" 'scala-eval-buffer)
-	(local-set-key "\C-csilf" 'scala-eval-load-file)
-	(local-set-key "\C-csiqi" 'scala-quit-interpreter)
-
-	(local-set-key "\M-c" 'compile)
-	(set (make-local-variable 'compile-command)
-	  (let ((fname (file-name-nondirectory buffer-file-name)))
-	    (if (or (file-exists-p "makefile") (file-exists-p "Makefile"))
-	      (concat "make " (file-name-sans-extension fname) ".class")
-	      (concat "fsc " fname)))))))
-
-
 ; scheme
 
   (add-to-list 'auto-mode-alist '("\\.(scm(awk\\|pic)?)\\|rkt$" . scheme-mode))
