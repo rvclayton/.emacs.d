57e0bf2fdf46fc195af0b6d32f0333d4fd64dd48
r. clayton <rvclayton@acm.org>
2016-03-20 21:59:08 -0400

    created

---
+++ pkgs.el
@@ -0,0 +1,13 @@
+(require 'use-package)
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :config
+    (yas-global-mode)
+    (add-hook 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
+    (setq yas-key-syntaxes '("w_" "w_." "^ "))
+    (setq yas-expand-only-for-last-commands nil)
+    (yas-global-mode 1)
+    (bind-key "\t" 'hippie-expand yas-minor-mode-map)
+    (add-to-list 'yas-prompt-functions 'shk-yas/helm-prompt))
