405bc66e8aa111ebc05c2611bdcbad15b5cba9c4
r. clayton <rvclayton@acm.org>
2017-08-13 20:34:22 -0400

    sequester emacs-managed customizations in a separate file.

--- init.el
+++ init.el
@@ -1,29 +1,16 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(canlock-password "ec29aa15f46d35b18a7e50e124673f00529cbd8f")
- '(initial-major-mode (quote text-mode))
- '(quack-programs
-   (quote
-    ("run-geiser" "mzscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
- '(send-mail-function (quote sendmail-send-it)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(button ((t (:inherit header\ line))))
- '(cursor ((t (:background "gold"))))
- '(custom-comment-tag ((t (:foreground "white"))))
- '(custom-link ((t (:inherit underline))))
- '(custom-visibility ((t (:inherit underline :height 0.8))))
- '(link ((t (:foreground "white" :underline t))))
- '(minibuffer-prompt ((t (:foreground "green"))))
- '(w3m-anchor ((((class color) (background dark)) (:foreground "yellow")) (((class color) (background light)) (:foreground "yellow"))))
- '(w3m-arrived-anchor ((((class color) (background dark)) (:foreground "cyan")) (((class color) (background light)) (:foreground "cyan")))))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-(load "~rclayton/.emacs.d/common.el")
-(load "~rclayton/.emacs.d/general.el")
+(let (f)
+
+  (fset 'f (lambda (f) (concat "~rclayton/.emacs.d/" f ".el")))
+  
+  (setq custom-file  (f "customizations"))
+
+  (load-file custom-file)
+  (load-file (f "common"))
+  (load-file (f "general")))
