d536988e622e874e6547fdb7d78c00b7c5455a20
r. clayton <rvclayton@acm.org>
2020-06-14 16:17:31 -0400

    neaten up yas

--- pkgs.el
+++ pkgs.el
@@ -72,14 +72,14 @@
   :ensure t
   :diminish yas-minor-mode
   :config
-    (yas-global-mode)
-    (add-hook 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
-    (setq yas-key-syntaxes '("w_" "w_." "^ "))
-    (setq yas-expand-only-for-last-commands nil)
-    (yas-global-mode 1)
-    (bind-key "\t" 'hippie-expand yas-minor-mode-map)
-    (add-to-list 'yas-prompt-functions 'shk-yas/helm-prompt)
-    (load "expansion-cursor-indicator"))
+  (yas-global-mode)
+  (add-hook 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
+  (setq yas-key-syntaxes '("w_" "w_." "^ "))
+  (setq yas-expand-only-for-last-commands nil)
+  (yas-global-mode 1)
+  (bind-key "\t" 'hippie-expand yas-minor-mode-map)
+  (add-to-list 'yas-prompt-functions 'shk-yas/helm-prompt)
+  (load "expansion-cursor-indicator"))
 
 
 (use-package yasnippet-snippets
